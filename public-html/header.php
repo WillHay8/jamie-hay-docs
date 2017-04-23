d7891212e1b9b49231f03aed02dc0367931ef0f9
Will Hay <williamfmhay@gmail.com>
2017-04-23 18:07:54 +0100

    fix favicon

--- public-html/header.php
+++ public-html/header.php
@@ -9,7 +9,7 @@ $description = isset($description)? $description : 'I’m a BAFTA award-winning 
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="shortcut icon" href="<?$root_url?>images/bafta4.png" type="image/x-icon" />
+		<link rel="shortcut icon" href="<?=$root_url?>images/bafta4.png" type="image/x-icon" />
 		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway|Alike Angular|Open Sans">
 		<link rel="stylesheet" type="text/css" href="<?=$root_url?>css/style.css">
 
